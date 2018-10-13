53341818a9b6f60480d9a465f49ba9daa79bb44b
stkay <stkayy@gmail.com>
2018-10-13 23:36:36 +0900

    とりあえず楽譜を表示する

--- src/content.ts
+++ src/content.ts
@@ -90,14 +90,28 @@ class ABCPage {
     private scoreElements: ScoreElement[] = [];
 
     private pushScoreElement = (block: ABCBlock): void => {
-        const div = document.createElement("div");
-        div.setAttribute("id", `ABC${block.titleElementID}`);
-        div.setAttribute("style", generateInlineStyle(block.isEditing, block.blockHeight));
-        block.titleElement.appendChild(div);
+        const {titleElementID, titleElement, blockHeight, abc, isEditing} = block;
+        const scoreDiv = document.createElement("div");
+        scoreDiv.setAttribute("id", `ABC${titleElementID}`);
+        scoreDiv.setAttribute("style", generateInlineStyle(isEditing, blockHeight));
+
+        const svgDiv = document.createElement("div");
+        const svgDivID = `SVG${titleElementID}`;
+        svgDiv.setAttribute("id", svgDivID);
+
+        const playerDiv = document.createElement("div");
+        const playerDivID = `PLAYER${titleElementID}`;
+        playerDiv.setAttribute("id", playerDivID);
+
+        scoreDiv.appendChild(svgDiv);
+        scoreDiv.appendChild(playerDiv);
+        block.titleElement.appendChild(scoreDiv);
         this.scoreElements.push({
-            parentElementID: block.titleElementID,
-            element: div
-        })
+            parentElementID: titleElementID,
+            element: scoreDiv
+        });
+        //楽譜描画
+        render(abc, parseLink(abc), svgDivID, playerDivID);
     };
 
     private getElement = (elementID: string): ScoreElement => {
@@ -116,6 +130,7 @@ class ABCPage {
             return false;
         }
         scoreElement.element.setAttribute("style", generateInlineStyle(block.isEditing, block.blockHeight));
+        //楽譜描画
         return true;
     };
 
@@ -130,6 +145,7 @@ class ABCPage {
 
 console.log(MSG, "hello from hyperscorebox");
 const page = new ABCPage();
+
 setInterval(async () => {
     const ABCIDs = getABCElIDs(await getPageLines());
     if (ABCIDs.length < 1) {
@@ -141,118 +157,117 @@ setInterval(async () => {
 }, 500);
 
 
-// const SCRAPBOXURL = "https://scrapbox.io/stk-study-music-theory/";
-// const LINK_HIGHLIGHT_COLOR = "#3965ff";
-// const inputEl = document.getElementById("abcinput");
-// let shifted: boolean = false;
-//
-// const parseLink = (abc: string): Link[] => {
-//     const parsedLinks: Link[] = [];
-//     const linkMached = abc.match(/%Links:.*/);
-//     if (!linkMached) {
-//         return [];
-//     }
-//     console.log("parseLink", "linkMatched", linkMached[0]);
-//
-//     const linkStr = linkMached[0].replace(/%Links:/, "");
-//     const links = linkStr.split("]");
-//     if (links.length < 1) {
-//         return [];
-//     }
-//
-//     for (let link of links) {
-//         link = link.replace("[", "");
-//         const arr = link.split(" ");
-//         if (arr.length < 2) continue;
-//         const numbersStr: string[] = arr[0].split(",");
-//         const numbers: number[] = numbersStr.map(str => Number(str));
-//         parsedLinks.push({startChars: numbers, pageTitle: arr[1]});
-//     }
-//
-//     console.log("parseLink", "parsedLinks", parsedLinks);
-//     return parsedLinks;
-// };
-//
-// const onInput = () => {
-//     console.log("inputEl", "oninput");
-//     const ABC: string = (inputEl as HTMLInputElement).value;
-//     render(ABC, inputEl as HTMLInputElement, parseLink(ABC));
-// };
-//
-// const addLinkToABC = (abc: string, startChar: number): string => {
-//     if (shifted) {
-//         const splitted = abc.split("]");
-//         const tail = splitted[splitted.length - 2];
-//         const updatedTail = tail.replace("[", `[${startChar},`);
-//         return abc.replace(tail, updatedTail);
-//     }
-//     if (/%Links:($|\[(.*|)\d+ .*]$)/.test(abc)) {
-//         return `${abc}[${startChar} new]`;
-//     }
-//     if (/\n$/.test(abc)) {
-//         return `${abc}%Links:[${startChar} new]`;
-//     }
-//     return `${abc}\n%Links:[${startChar} new]`;
-// };
-//
-// const generateClickListener = (inputEl: HTMLInputElement, links: Link[]) => {
-//     return (abcElem, tuneNumber, classes): void => {
-//         console.log("abcClickListener", abcElem, tuneNumber, classes);
-//         //abcjsによる赤ハイライトを取り消す
-//         abcElem.abselem.highlight(undefined, LINK_HIGHLIGHT_COLOR);
-//
-//         const clickedNoteStartChar: number = abcElem.startChar;
-//         const pageTitle: string | null = getLink(links, clickedNoteStartChar);
-//         if (pageTitle) {
-//             console.log("abcClickListener", "Linked note is clicked.", "startChar:", clickedNoteStartChar, "destination:", pageTitle);
-//             window.open(SCRAPBOXURL + pageTitle);
-//             return
-//         }
-//
-//         const ABC = inputEl.value;
-//         inputEl.value = addLinkToABC(ABC, clickedNoteStartChar);
-//         onInput();
-//
-//     };
-// };
-//
-// const getLink = (links: Link[], startChar: number): string | null => {
-//     for (let link of links) {
-//         for (let _startChar of link.startChars) {
-//             if (_startChar === startChar) {
-//                 return link.pageTitle
-//             }
-//         }
-//     }
-//     return null
-// };
-//
-// const render = (abc: string, inputEl: HTMLInputElement, links: Link[]): void => {
-//     const options = {
-//         clickListener: generateClickListener(inputEl, links),
-//         add_classes: true,
-//         staffwidth: 1024
-//     };
-//     const tuneObjectArray = abcjs.renderAbc("svgoutput", abc, options);
-//     console.log("render", "tuneObjectArray", tuneObjectArray);
-//
-//     //リンクをハイライト
-//     const voices = tuneObjectArray[0].lines[0].staff[0].voices;
-//     for (let voice of voices) {
-//         for (let element of voice) {
-//             if (!element.startChar) continue;
-//             if (getLink(links, Number(element.startChar))) {
-//                 element.abselem.highlight(undefined, LINK_HIGHLIGHT_COLOR);
-//             }
-//         }
-//     }
-//
-//     abcjs.renderMidi("midioutput", abc, {inlineControls: {loopToggle: true,}});
-// };
-//
-// window.addEventListener("mousedown", e => {
-//     console.log("onmousedown", "shifted:", e.shiftKey);
-//     shifted = e.shiftKey
-// });
+const SCRAPBOXURL = "https://scrapbox.io/stk-study-music-theory/";
+const LINK_HIGHLIGHT_COLOR = "#3965ff";
+const inputEl = document.getElementById("abcinput");
+let shifted: boolean = false;
+
+const parseLink = (abc: string): Link[] => {
+    const parsedLinks: Link[] = [];
+    const linkMached = abc.match(/%Links:.*/);
+    if (!linkMached) {
+        return [];
+    }
+    console.log("parseLink", "linkMatched", linkMached[0]);
+
+    const linkStr = linkMached[0].replace(/%Links:/, "");
+    const links = linkStr.split("]");
+    if (links.length < 1) {
+        return [];
+    }
+
+    for (let link of links) {
+        link = link.replace("[", "");
+        const arr = link.split(" ");
+        if (arr.length < 2) continue;
+        const numbersStr: string[] = arr[0].split(",");
+        const numbers: number[] = numbersStr.map(str => Number(str));
+        parsedLinks.push({startChars: numbers, pageTitle: arr[1]});
+    }
+
+    console.log("parseLink", "parsedLinks", parsedLinks);
+    return parsedLinks;
+};
+
+const onInput = () => {
+    console.log("inputEl", "oninput");
+    const ABC: string = (inputEl as HTMLInputElement).value;
+    // render(ABC, inputEl as HTMLInputElement, parseLink(ABC));
+};
+
+const addLinkToABC = (abc: string, startChar: number): string => {
+    if (shifted) {
+        const splitted = abc.split("]");
+        const tail = splitted[splitted.length - 2];
+        const updatedTail = tail.replace("[", `[${startChar},`);
+        return abc.replace(tail, updatedTail);
+    }
+    if (/%Links:($|\[(.*|)\d+ .*]$)/.test(abc)) {
+        return `${abc}[${startChar} new]`;
+    }
+    if (/\n$/.test(abc)) {
+        return `${abc}%Links:[${startChar} new]`;
+    }
+    return `${abc}\n%Links:[${startChar} new]`;
+};
+
+const generateClickListener = (inputEl: HTMLInputElement, links: Link[]) => {
+    return (abcElem, tuneNumber, classes): void => {
+        console.log("abcClickListener", abcElem, tuneNumber, classes);
+        //abcjsによる赤ハイライトを取り消す
+        abcElem.abselem.highlight(undefined, LINK_HIGHLIGHT_COLOR);
+
+        const clickedNoteStartChar: number = abcElem.startChar;
+        const pageTitle: string | null = getLink(links, clickedNoteStartChar);
+        if (pageTitle) {
+            console.log("abcClickListener", "Linked note is clicked.", "startChar:", clickedNoteStartChar, "destination:", pageTitle);
+            window.open(SCRAPBOXURL + pageTitle);
+            return
+        }
+
+        const ABC = inputEl.value;
+        inputEl.value = addLinkToABC(ABC, clickedNoteStartChar);
+        onInput();
+    };
+};
+
+const getLink = (links: Link[], startChar: number): string | null => {
+    for (let link of links) {
+        for (let _startChar of link.startChars) {
+            if (_startChar === startChar) {
+                return link.pageTitle
+            }
+        }
+    }
+    return null
+};
+
+const render = (abc: string, links: Link[], svgDivID: string, playerDivID: string): void => {
+    const options = {
+        // clickListener: generateClickListener(inputEl, links),
+        add_classes: true,
+        staffwidth: 1024
+    };
+    const tuneObjectArray = abcjs.renderAbc(svgDivID, abc, options);
+    console.log("render", "tuneObjectArray", tuneObjectArray);
+
+    //リンクをハイライト
+    const voices = tuneObjectArray[0].lines[0].staff[0].voices;
+    for (let voice of voices) {
+        for (let element of voice) {
+            if (!element.startChar) continue;
+            if (getLink(links, Number(element.startChar))) {
+                element.abselem.highlight(undefined, LINK_HIGHLIGHT_COLOR);
+            }
+        }
+    }
+
+    abcjs.renderMidi(playerDivID, abc, {inlineControls: {loopToggle: true,}});
+};
+
+window.addEventListener("mousedown", e => {
+    console.log("onmousedown", "shifted:", e.shiftKey);
+    shifted = e.shiftKey
+});
 // inputEl.addEventListener("input", onInput);
 // onInput();
